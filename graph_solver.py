4652d205b01e23db4c0ebfe2b3f192b14195e8f3
taliadym <talia.dym@mail.huji.ac.il>
2024-09-03 23:42:13 +0300

    building the solver structure

--- graph_solver.py
+++ graph_solver.py
@@ -6,8 +6,11 @@ class GraphSolver:
         self.src_node = src_node
         self.dest_node = dest_node
 
-        # this holds the current path - DIRECTED edge
-        self.current_d_edge = None
+        # where the car is currently
+        self.current_node = self.src_node
+
+        # this holds the current path - sorted list of DIRECTED edges from current_node to dest_node
+        self.current_d_path = []
 
         # this holds the traffic index for each edge (0-no traffic, 1-full traffic), should be in (0, 1) - NOT 0, 1
         self.traffic_index = {edge: 0 for edge in self.edges}
