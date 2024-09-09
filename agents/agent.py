0b50ee4e9b535b562d84139a63c6e3561e652aaf
taliadym <talia.dym@mail.huji.ac.il>
2024-09-09 18:14:09 +0300

    documentation

--- agents/agent.py
+++ agents/agent.py
@@ -2,18 +2,18 @@
 class Agent:
     def __init__(self, goal_node):
         """
-        Initialize the agent with a start node and a goal node.
-
-        :param start_node: The starting node of the agent.
+        Initialize the agent with a goal node.
         :param goal_node: The goal node the agent aims to reach.
         """
         self.goal_node = goal_node
         self.path = []
 
-    def __str__(self):
-        """String representation of the agent's current status."""
-        return f"Agent at {self.current_node}, Goal: {self.goal_node}, Path: {self.path}"
 
-    def find_path(self, start_node):
+    def find_path(self, start_node, edge_costs):
+        """
+        Find best path from start node to dest node taking the cost into account
+        :param start_node: The source node the agent starts from.
+        :param edge_costs: A dictionary of with edges as keys and the time cost to cross the edge as values..
+        """
         return None
 
